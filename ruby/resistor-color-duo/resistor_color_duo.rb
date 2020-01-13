c1d3ebfe1e22422ae9cc75b6162fa9a127355f36
naosuke <info@naosuke.me>
2020-01-13 22:55:13 +0900

    ruby/resistor-color-duo: solved

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,28 @@
+=begin
+Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
+`resistor_color_duo_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color-duo` directory.
+=end
+
+class ResistorColorDuo
+  def self.value(array)
+    array.map{ |c| COLORS[c.to_sym] }.shift(2).join.to_i
+  end
+
+  private
+
+  COLORS = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }.freeze
+end
